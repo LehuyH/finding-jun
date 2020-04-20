fc388383742f50adc3a2d39a49b95b09fd00cf0b
LehuyH <lehuy.hoang@outlook.com>
2020-04-20 11:28:14 -0700

    misc fixes

--- js/audio.js
+++ js/audio.js
@@ -1,18 +1,18 @@
 var sfx = {
     hover: new Howl({
-        src: ['../audio/sfx/move.mp3']
+        src: ['audio/sfx/move.mp3']
     }),
     confirm: new Howl({
-        src: ['../audio/sfx/Menu Confirm.mp3']
+        src: ['audio/sfx/Menu Confirm.mp3']
     }),
     transition: new Howl({
-        src: ['../audio/sfx/Transition.mp3']
+        src: ['audio/sfx/Transition.mp3']
     }),
     lvlUp: new Howl({
-        src: ['../audio/sfx/Level Up.mp3']
+        src: ['audio/sfx/Level Up.mp3']
     }),
     blip: new Howl({
-        src: ['../audio/sfx/blip.wav'],
+        src: ['audio/sfx/blip.wav'],
         loop:true
     })
 }
